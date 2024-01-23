b03a0d04799e02dfe003b65bdc0ce6cebe63c916
EugeneKru <rain.hopes.rain@gmail.com>
2024-01-23 17:05:12 +0300

    Solution hometask03. Another way to remove duplicates

--- src/eugenekru/javacourse/arraystasks/hometask03/RemoveArrayDuplicates.java
+++ src/eugenekru/javacourse/arraystasks/hometask03/RemoveArrayDuplicates.java
@@ -9,20 +9,26 @@ public class RemoveArrayDuplicates {
 	public static void main(String[] args) {
 		
 		int[] originalarray = MyArraysMethods.createRandomArray(15, -5, 5);
-		int duplicates = countDuplicateNumbers(originalarray);
-		int[] cleararray = new int[originalarray.length - duplicates];
+		int[] interimarray = new int[originalarray.length];
 		int index = 0;
 		
-		for(int i = 0; i < cleararray.length; i++) {
+		for(int i = 0, j; i < interimarray.length; i++) {
 			
-			for (int j = index; j < originalarray.length; j++) {
+			for (j = index; j < originalarray.length; j++) {
 				if(isUniqNumber(originalarray, j)) {
-					cleararray[i] = originalarray[j];
+					interimarray[i] = originalarray[j];
 					index = j + 1;
 					break;
 				} 
-			}			
+			}
+			
+			if(j == (originalarray.length - 1)) {
+				index = i; 
+				break;
+			}
 		}
+		
+		int[] cleararray = Arrays.copyOf(interimarray, (index + 1));
 		
 		System.out.println("Original Array:");
 		MyArraysMethods.printArray(originalarray);
@@ -30,22 +36,7 @@ public class RemoveArrayDuplicates {
 		MyArraysMethods.printArray(cleararray);
 
 	}
-	
-	private static int countDuplicateNumbers(int[] array) {
 		
-		int count = 0;
-		int[] sortarray = Arrays.copyOf(array, array.length);		
-		Arrays.sort(sortarray);
-		
-		for(int i = 1; i < sortarray.length; i++) {
-			if(sortarray[i] == sortarray[i - 1]) {
-				count++;
-			}
-		}
-		
-		return count;		
-	}
-	
 	private static boolean isUniqNumber(int[] array, int index) {
 		
 		boolean uniq = true;		
