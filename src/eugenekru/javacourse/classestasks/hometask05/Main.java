d0309d682e203f27d5c6dcc1baebbeeb8b88c137
EugeneKru <rain.hopes.rain@gmail.com>
2024-02-10 16:37:47 +0300

    solution task05

---
+++ src/eugenekru/javacourse/classestasks/hometask05/Main.java
@@ -0,0 +1,31 @@
+package eugenekru.javacourse.classestasks.hometask05;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Phone cat = new Phone("+375297777777", "Google", 197);
+		Phone dog = new Phone("+375295555555", "Apple", 228);
+		Phone cow = new Phone("+375293333333", "Nokia", 98);
+		
+		System.out.println(cat + "\n" + dog + "\n" + cow);
+		
+		cat.recieveCall("Шарик");
+		System.out.println(dog.getNumber());
+		
+		dog.recieveCall("Гаврюша");
+		System.out.println(cow.getNumber());
+		
+		cow.recieveCall("Матроскин");
+		System.out.println(cat.getNumber());
+		
+		cat.recieveCall("Шарик", dog.getNumber());
+		dog.recieveCall("Гаврюша", cow.getNumber());
+		cow.recieveCall("Матроскин", cat.getNumber());
+		
+		cat.sendMessage(dog.getNumber(), cow.getNumber());
+		dog.sendMessage(cat.getNumber());
+		cow.sendMessage(dog.getNumber(), cat.getNumber());		
+	}
+
+}
---
+++ src/eugenekru/javacourse/classestasks/hometask05/Phone.java
@@ -0,0 +1,43 @@
+package eugenekru.javacourse.classestasks.hometask05;
+
+public class Phone {
+	
+	private String number;
+	
+	private String model;
+	
+	private double weight;
+	
+	
+	public Phone(String number, String model, double weigth) {
+		
+		this.number = number;
+		this.model = model;
+		this.weight = weigth;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+	
+	public void recieveCall(String name) {		
+		System.out.println("\nЗвонит " + name);
+	}
+	
+	public void recieveCall(String name, String number) {		
+		System.out.println("\nЗвонит " + name + "\n" + number);
+	}
+	
+	public void sendMessage(String...number) {
+		
+		System.out.println("\nThe message will be sent from number " + this.number + " to:");
+		for(int i = 0; i < number.length; i++) {
+			System.out.println(number[i]);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Phone number: " + number + "\nmodel: " + model + "\nweigth: " + weight + "\n";
+	}
+}
