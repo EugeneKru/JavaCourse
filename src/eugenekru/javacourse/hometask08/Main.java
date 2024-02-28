7a8cf2d8ba2d2dd9cd89607737251951e0f228af
EugeneKru <rain.hopes.rain@gmail.com>
2024-02-28 16:57:34 +0300

    solution hometask #8

---
+++ src/eugenekru/javacourse/hometask08/Main.java
@@ -0,0 +1,14 @@
+package eugenekru.javacourse.hometask08;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyMap map = new MyMap();
+		
+		map.getCharsCount("qwert qwer qwe qw q");		
+		
+		System.out.println(map.toString());
+	}	
+
+}
