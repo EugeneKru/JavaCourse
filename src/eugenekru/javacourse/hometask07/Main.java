97084e93b62fda0728f6e00ae5a31690ec93fc0f
EugeneKru <rain.hopes.rain@gmail.com>
2024-03-11 17:39:02 +0300

    all comments fixed

--- src/eugenekru/javacourse/hometask07/Main.java
+++ src/eugenekru/javacourse/hometask07/Main.java
@@ -4,20 +4,44 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Pair<Integer> numbersPair = new Pair<>(333, 555);
-		System.out.println(numbersPair);			
-		numbersPair.demoPairClassMethods(999, 111);
-				
-		Pair<String> stringsPair = new Pair<>("first", "last");		
-		System.out.println(stringsPair);		
-		stringsPair.demoPairClassMethods("apple", "pear");		
+		Pair<Integer, String> numberString1 = new Pair<>(999, "first");		
+		demoPairClassMethods(numberString1, 111, "last");
 		
-		Pair<Character> charsPair = new Pair<>('A', 'B');
-		Pair<Boolean> booleansPair = new Pair<>(true, false);
+		Pair<Boolean, Character> booleanCharacter1 = new Pair<>(true, 'A');
+		demoPairClassMethods(booleanCharacter1, false, 'Z');
 		
-		Pair<Pair> pairPair = new Pair<>(numbersPair, stringsPair);		
-		System.out.println(pairPair);		
-		pairPair.demoPairClassMethods(charsPair, booleansPair);
+		
+		Pair<Integer, String> numberString2 = new Pair<>(777, "good");		
+		Pair<Boolean, Character> booleanCharacter2 = new Pair<>(false, '!');
+		
+		Pair<Pair<Integer, String>,
+			 Pair<Boolean, Character>> twoPairs = new Pair<>(numberString1,
+					 										 booleanCharacter1);		
+		demoPairClassMethods(twoPairs, numberString2, booleanCharacter2);
+
 	}
+	
+	public static <T1, T2> void demoPairClassMethods(Pair<T1, T2> pair,
+													 T1 newFirstElement,
+													 T2 newLastElement)
+	{	
+		Pair<T1, T2> pairDemo = new Pair<>(pair.first(), pair.last());
+		
+		System.out.println("First element of Pair is "
+		+ pairDemo.first().getClass().getSimpleName()
+		+ ": " + pairDemo.first() + "\n");
+		
+		System.out.println("LastElement of Pair is "
+		+ pairDemo.last().getClass().getSimpleName()
+		+ ": " + pairDemo.last() + "\n");		
+		
+		pairDemo.replaceFirstElement(newFirstElement);
+		System.out.println("Pair after replace first element\n" + pairDemo);
+		
+		pairDemo.replaceLastElement(newLastElement);
+		System.out.println("Pair after replace last element\n" + pairDemo);		
+		
+		System.out.println("Pair after swap\n" + pairDemo.swap());
+		}	
 
 }
--- src/eugenekru/javacourse/hometask07/Pair.java
+++ src/eugenekru/javacourse/hometask07/Pair.java
@@ -1,70 +1,45 @@
 package eugenekru.javacourse.hometask07;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-public class Pair<T> {
+public class Pair<T1, T2> {
 	
-	private String className;
+	private T1 element1;
 	
-	private List<T> pairList = new ArrayList<>();
+	private T2 element2;
 
-	public Pair(T element1, T element2) {
-		this.pairList.add(element1);
-		this.pairList.add(element2);
-		this.className = element1.getClass().getSimpleName();
+	public Pair(T1 element1, T2 element2) {		
+		this.element1 = element1;
+		this.element2 = element2;		
 	}	
 	
-	private T first() {
-		return this.pairList.get(0);
-	}	
-	
-	private T last() {
-		return this.pairList.get(pairList.size() - 1);
-	}	
-	
-	private void swap() {
-		Collections.reverse(pairList);		
+	public T1 first() {
+		return element1;
 	}
 	
+	public T2 last() {
+		return element2;
+	}
 	
-	private void replaceFirst(T element) {		
-		pairList.set(0, element);
+	public Pair<T2, T1> swap() {		
+		return new Pair<>(this.element2, this.element1);		
+	}
+	
+	public void replaceFirstElement(T1 element) {		
+		this.element1 = element;		
 	}	
 	
-	private void replaceLast(T element) {		
-		pairList.set((pairList.size() - 1), element);
-	}	
-	
-	public void demoPairClassMethods(T element1, T element2) {
-		
-		T firstElement = first();
-		T lastElement = last();
-		Pair<T> pairDemo = new Pair<>(firstElement, lastElement);		
-		System.out.println(className + "s first element: "
-									 + pairDemo.first() + "\n");
-		
-		System.out.println(className + "s last element: "
-									 + pairDemo.last() + "\n");
-		
-		pairDemo.swap();
-		System.out.println(pairDemo.replaceFirst(" after swap:"));
-		
-		pairDemo.replaceFirst(element1);
-		System.out.println(pairDemo.replaceFirst(" after replace first element:"));
-		
-		pairDemo.replaceLast(element2);
-		System.out.println(pairDemo.replaceFirst(" after replace last element:"));		
+	public void replaceLastElement(T2 element) {		
+		this.element2 = element;
 	}
 
 	@Override
-	public String toString() {
-		return "Pair of " + className + "s:\n" + pairList + "\n";
-	}
-	
-	public String replaceFirst(String string) {
-		return toString().replaceFirst(":", string);
-	}
+	public String toString() {		
+		return "FirstElement of Pair is "
+				+ element1.getClass().getSimpleName()
+				+ ": " + element1 + "\n"
+				+ "LastElement of Pair is "
+				+ element2.getClass().getSimpleName()
+				+ ": " + element2 + "\n";
+	}	
 
 }
